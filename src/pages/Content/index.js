3211785fe70584117c6b7f4a0153a496fa7b869b
ridoy <majumdar.ridoy@gmail.com>
2023-12-31 14:36:43 -0500

    add mutationobserver for tweet stats

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,3 +1,4 @@
+import { remove } from 'fs-extra';
 import { printLine } from './modules/print';
 
 console.log('Content script works!');
@@ -7,12 +8,27 @@ printLine("Using the 'printLine' function from the Print Module");
 
 
 // Remove all like, comment, retweet, and view counts from Twitter/X
-function removeTweetStats() {
-    console.log("hi");
-    document
-        .querySelectorAll('.css-175oi2r.r-xoduu5.r-1udh08x')
-        .forEach((element) => element.remove())
+
+let targetNode;
+
+let intervalId = setInterval(() => {
+    targetNode = document.querySelector(".css-175oi2r.r-1jgb5lz.r-13qz1uu.r-1ye8kvj");
+    if (targetNode) {
+        clearInterval(intervalId);
+        initMutationObserver();
+    }
+}, 1000);
+
+function initMutationObserver() {
+    console.log(targetNode);
+    const config = { attributes: true, childList: true, subtree: true };
+    function removeTweetStats(mutationList, observer) {
+        console.log("hi");
+        document
+            .querySelectorAll('.css-175oi2r.r-xoduu5.r-1udh08x')
+            .forEach((element) => element.remove())
+    };
+
+    const observer = new MutationObserver(removeTweetStats)
+    observer.observe(targetNode, config);
 }
-setInterval(removeTweetStats, 1000);
-
-
