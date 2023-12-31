1931698976e194a18114fa246b8c036fb7726e57
ridoy <majumdar.ridoy@gmail.com>
2023-12-31 13:58:16 -0500

    remove tweet stats

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -4,3 +4,15 @@ console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
 printLine("Using the 'printLine' function from the Print Module");
+
+
+// Remove all like, comment, retweet, and view counts from Twitter/X
+function removeTweetStats() {
+    console.log("hi");
+    document
+        .querySelectorAll('.css-175oi2r.r-xoduu5.r-1udh08x')
+        .forEach((element) => element.remove())
+}
+setInterval(removeTweetStats, 1000);
+
+
