671f1f5f20de614c4d189463070dbb480b3167e0
ridoy <majumdar.ridoy@gmail.com>
2024-01-03 01:19:07 -0500

    premature optimization

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,49 +1,70 @@
-import { printLine } from './modules/print';
 import { youtube } from './modules/youtube';
 import { twitter } from './modules/twitter';
 
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
+console.log('webzen content script has loaded.');
 
-printLine("Using the 'printLine' function from the Print Module");
-
-
-// Remove all like, comment, retweet, and view counts from Twitter/X
-
-let homeTimelineNode;
-let userTimelineNode;
 const HOME_TIMELINE_SELECTOR = '.css-175oi2r.r-1jgb5lz.r-13qz1uu.r-1ye8kvj';
 const USER_TIMELINE_SELECTOR = '.css-175oi2r.r-16y2uox.r-1wbh5a2.r-1habvwh';
+const TRENDING_SELECTOR = '.css-175oi2r.r-1867qdf.r-1phboty.r-rs99b7.r-1ifxtd0.r-1udh08x.r-g2wdr4.r-14wv3jr';
 const TWEET_STATS_SELECTOR = '.css-175oi2r.r-xoduu5.r-1udh08x';
+const RELATED_VIDEOS_SELECTOR = "#related";
+const MAX_RETRY_COUNT = 100; // 100s
 
-waitForElementCreationAndRemoveTweetStats(HOME_TIMELINE_SELECTOR);
-waitForElementCreationAndRemoveTweetStats(USER_TIMELINE_SELECTOR);
+waitForElementsCreation(HOME_TIMELINE_SELECTOR)
+    .then((homeTimelineNode) => {
+        observeAndRemoveChildElements(homeTimelineNode[0], TWEET_STATS_SELECTOR);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
-function waitForElementCreationAndRemoveTweetStats(selector) {
-    let intervalId = setInterval(() => {
-        let targetNode = document.querySelector(selector);
-        if (targetNode) {
-            clearInterval(intervalId);
-            initMutationObserver(TWEET_STATS_SELECTOR, targetNode);
-        }
-    }, 1000);
+waitForElementsCreation(USER_TIMELINE_SELECTOR)
+    .then((userTimelineNode) => {
+        observeAndRemoveChildElements(userTimelineNode[0], TWEET_STATS_SELECTOR);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+waitForElementsCreation(TRENDING_SELECTOR)
+    .then((trendingNode) => {
+        trendingNode.forEach(node => node.remove());
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+waitForElementsCreation(RELATED_VIDEOS_SELECTOR)
+    .then((relatedVideosNode) => {
+        relatedVideosNode.forEach(node => node.remove());
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+function waitForElementsCreation(selector) {
+    return new Promise((resolve, reject) => {
+        let retryCount = 0;
+        let intervalId = setInterval(() => {
+            let elements = document.querySelectorAll(selector);
+            if (elements.length > 0) {
+                clearInterval(intervalId);
+                resolve(elements);
+            }
+            retryCount++;
+            if (retryCount > MAX_RETRY_COUNT) {
+                reject(`No element at ${selector} was found.`);
+            }
+        }, 1000);
+    });
 }
 
-function initMutationObserver(selector, targetNode) {
+// TODO better name
+function observeAndRemoveChildElements(parentNode, childElementSelector) {
     const config = { attributes: true, childList: true, subtree: true };
-    function removeTweetStats() {
-        document
-            .querySelectorAll(selector)
-            .forEach((element) => element.remove())
-    };
-
-    const observer = new MutationObserver(removeTweetStats)
-    observer.observe(targetNode, config);
-}
-
-
-// Remove related section from YouTube
-if (window.location.href.indexOf("youtube.com") !== -1) {
-    const relatedVids = document.getElementById('related');
-    if (relatedVids) relatedVids.remove()
+    const observer = new MutationObserver(() => {
+        document.querySelectorAll(childElementSelector)
+            .forEach((element) => element.remove());
+    })
+    observer.observe(parentNode, config);
 }
